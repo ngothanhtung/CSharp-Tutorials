e222cca765c0bcc503e943981199b33169f3105d
Ngo Thanh Tung <tungnt@softech.vn>
2026-03-16 18:37:13 +0700

    Add MVC project

---
+++ 2.examples/ConsoleApplication1/OnlineShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
