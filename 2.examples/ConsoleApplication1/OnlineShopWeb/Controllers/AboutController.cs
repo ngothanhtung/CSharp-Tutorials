18b6b99a4a4855b25e4d21436ba8f7855d594aad
Ngo Thanh Tung <tungnt@softech.vn>
2026-03-25 20:36:24 +0700

    Update: Tag Helper

--- 2.examples/ConsoleApplication1/OnlineShopWeb/Controllers/AboutController.cs
+++ 2.examples/ConsoleApplication1/OnlineShopWeb/Controllers/AboutController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
+using OnlineShopWeb.Models;
 
 namespace OnlineShopWeb.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly OnlineShopDataContext _context;
+        public AboutController(OnlineShopDataContext context)
+        {
+            _context = context;
+        }
+
         // https://localhost:5001/About
         [HttpGet]
         public IActionResult Index()
@@ -37,6 +46,96 @@ namespace OnlineShopWeb.Controllers
             });
 
             return View(customers);
+        }
+
+        public IActionResult MoreDetails()
+        {
+            return View();
+        }
+
+        //[Route("About/Company/Vietnam/Address", Name = "address")]
+        
+        public IActionResult Address()
+        {
+            var model = new RegisterViewModel()
+            {
+                Email = "",
+                Password = "",
+                Country = "VN",
+
+            };
+
+
+
+            var countries = new List<SelectListItem>();
+            countries.AddRange([
+                new SelectListItem { Value = "VN", Text = "Vietnam" },
+                new SelectListItem() { Value = "MX", Text = "Mexico" },
+                new SelectListItem() { Value = "CA", Text = "Canada" },
+                new SelectListItem() { Value = "US", Text = "USA" }
+            ]);
+
+            ViewBag.Countries = countries;
+
+
+            // Get categories from database
+            var categoriesFromDatabase = this._context.Categories.ToList();
+
+            var categories = new List<SelectListItem>();
+            foreach (var category in categoriesFromDatabase)
+            {
+                categories.Add(new SelectListItem
+                {
+                    Value = category.Id.ToString(),
+                    Text = category.Name
+                });
+            }
+
+            ViewBag.Categories = categories;
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        //[Route("About/Company/Vietnam/Address", Name = "address")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Address([Bind("Email,Password,Age,Country,CategoryId")] RegisterViewModel data)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Không hợp lệ
+
+            var countries = new List<SelectListItem>();
+            countries.AddRange([
+                new SelectListItem { Value = "VN", Text = "Vietnam" },
+                new SelectListItem() { Value = "MX", Text = "Mexico" },
+                new SelectListItem() { Value = "CA", Text = "Canada" },
+                new SelectListItem() { Value = "US", Text = "USA" }
+            ]);
+
+            ViewBag.Countries = countries;
+
+
+            // Get categories from database
+            var categoriesFromDatabase = this._context.Categories.ToList();
+
+            var categories = new List<SelectListItem>();
+            foreach (var category in categoriesFromDatabase)
+            {
+                categories.Add(new SelectListItem
+                {
+                    Value = category.Id.ToString(),
+                    Text = category.Name
+                });
+            }
+
+            ViewBag.Categories = categories;
+
+            return View(data);
         }
     }
 }
---
+++ 2.examples/ConsoleApplication1/OnlineShopWeb/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OnlineShopWeb.Models;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Thư điện tử bắt buộc phải nhập")]
+    [EmailAddress(ErrorMessage = "Thư điện tử không hợp lệ")]
+    [Display(Name = "Thư điện tử")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Mật khẩu không được để trống")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Mật khẩu")]
+    public required string Password { get; set; }
+
+    [Required(ErrorMessage = "Tuổi không được để trống")]
+    [Range(18, 60, ErrorMessage = "Tuổi không hợp lệ, tuổi phải trong khoảng 18 đến 60")]
+    public int Age { get; set; }
+
+    public string Country { get; set; } = "VN";
+
+    public int? CategoryId { get; set; }
+
+}
